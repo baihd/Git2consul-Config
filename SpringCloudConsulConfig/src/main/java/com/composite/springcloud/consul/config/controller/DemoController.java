7578a70a6843b2c648e6ea9afcd17a7228ed67b7
柏会东 <847350737@qq.com>
2019-06-13 10:52:01 +0800

    增加files格式的consul配置,增加健康检查

--- SpringCloudConsulConfig/src/main/java/com/composite/springcloud/consul/config/controller/DemoController.java
+++ SpringCloudConsulConfig/src/main/java/com/composite/springcloud/consul/config/controller/DemoController.java
@@ -1,10 +1,12 @@
 package com.composite.springcloud.consul.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class DemoController {
 
     @Value("${first.name}")
@@ -14,4 +16,5 @@ public class DemoController {
     public String firstName() {
         return firstName;
     }
+
 }
